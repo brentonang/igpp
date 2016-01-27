47074f30b11d03cfd1ede3092b0b7082387dc9cc
Brenton Ang <notnerb.8808@gmail.com>
2016-01-27 04:05:03 -0800

    Removed option to ask for user input for vector size. Vector size now determined using algorithm.

--- ccode/main/CompareTopo_AlongPath.cpp
+++ ccode/main/CompareTopo_AlongPath.cpp
@@ -16,74 +16,71 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    ofstream myFile;
-    int i = 0, j = 0, vector_size;
-    while (vector_size < 1302) { // vector size will be floor[(max_range+rinc)/rinc]+1
-        cout << "Please enter a vector size (must be at least 1302): ";
-        cin >> vector_size;
-        if (vector_size < 1302) cout << "Please enter a value at least 1302" << endl;
-    }
-    double azimuth, maxrange, max_range, range = 160000, rinc, scalar1, scalar2, srclat = 41.131, srclon = 360 - 112.8965;
-    vector<double> flatlat(vector_size), flatlon(vector_size), olat(vector_size), olon(vector_size), plat(vector_size), plon(vector_size), ranges(vector_size), tempRanges(vector_size), vector1(vector_size), vector2(vector_size);
-    pair<vector<double>, vector<double>> tempValues;
-    pair<vector<double>, vector<double>> returnPair;
-    // tuple<vector<double>, vector<double>, vector<double>> returnTuple;
+   ofstream myFile;
+   double azimuth, maxrange, max_range, range = 160000, rinc, scalar1, scalar2, srclat = 41.131, srclon = 360 - 112.8965;
+   int i = 0, j = 0;
 
-    if(argc != 4) {
-        cout << "Invalid number of command line arguments" << endl;
-        return -1;
-    } else {
-        rinc = atof(argv[1]);
-        if (rinc <= 30) {
-            cout << "rinc should be greater than 30m" << endl;
-            return -1;
-        }
-        azimuth = atof(argv[2]);
-        if (azimuth < 0 || azimuth > 360) {
-            cout << "azimuth should be between 0 and 360" << endl;
-            return -1;
-        }
-        maxrange = atof(argv[3]);
-        if (maxrange < 0 || maxrange > 150000) {
-            cout << "maxrange should be between 0m and 150000m" << endl;
-            return -1;
-        }
-    }
+   if(argc != 4) {
+      cout << "Invalid number of command line arguments" << endl;
+      return -1;
+   } else {
+      rinc = atof(argv[1]);
+      if (rinc <= 30) {
+         cout << "rinc should be greater than 30m" << endl;
+         return -1;
+      }
+      azimuth = atof(argv[2]);
+      if (azimuth < 0 || azimuth > 360) {
+         cout << "azimuth should be between 0 and 360" << endl;
+         return -1;
+      }
+      maxrange = atof(argv[3]);
+      if (maxrange < 0 || maxrange > 150000) {
+         cout << "maxrange should be between 0m and 150000m" << endl;
+         return -1;
+      }
+   }
+   
+   int vector_size = floor((maxrange + rinc) / rinc) + 1;
+   vector<double> flatlat(vector_size), flatlon(vector_size), olat(vector_size), olon(vector_size), plat(vector_size), plon(vector_size), ranges(vector_size), tempRanges(vector_size), vector1(vector_size), vector2(vector_size);
+   pair<vector<double>, vector<double>> tempValues;
+   pair<vector<double>, vector<double>> returnPair;
+   // tuple<vector<double>, vector<double>, vector<double>> returnTuple;
 
-    max_range = min(maxrange, range);
-    while(j <= max_range + rinc) {
-        ranges[i] += j;
-        j += rinc;
-        i++;
-    } 
+   max_range = min(maxrange, range);
+   while(j <= max_range + rinc) {
+      ranges[i] += j;
+      j += rinc;
+      i++;
+   } 
     
-    if(azimuth < 0 || azimuth > 180) {
-        cout << "ERROR: Azimuth must be eastward" << endl;
-        return -1;
-    }
+   if(azimuth < 0 || azimuth > 180) {
+      cout << "ERROR: Azimuth must be eastward" << endl;
+      return -1;
+   }
 
-    vector<double> azm(ranges.size(), 1 * azimuth);
-    tempRanges = elementWiseDivision(ranges, 1852);
-    tempValues = cGetLatLon(srclat, srclon, tempRanges, azm, 6378206.4, 0.006768658);
-    plat = tempValues.first;
-    plon = tempValues.second;
-    for (i = 0; i < plon.size(); i++) plon[i] = cSetMinMax(plon[i], 0, 360);
-    for (i = 0; i < ranges.size(); i++) flatlon[i] = srclon + sin(azimuth * deg2rad) * ranges[i] / (cos(srclat * deg2rad) * 111111);
-    for (i = 0; i < ranges.size(); i++) flatlat[i] = srclat + cos(azimuth * deg2rad) * ranges[i] / 111111;
+   vector<double> azm(ranges.size(), 1 * azimuth);
+   tempRanges = elementWiseDivision(ranges, 1852);
+   tempValues = cGetLatLon(srclat, srclon, tempRanges, azm, 6378206.4, 0.006768658);
+   plat = tempValues.first;
+   plon = tempValues.second;
+   for (i = 0; i < plon.size(); i++) plon[i] = cSetMinMax(plon[i], 0, 360);
+   for (i = 0; i < ranges.size(); i++) flatlon[i] = srclon + sin(azimuth * deg2rad) * ranges[i] / (cos(srclat * deg2rad) * 111111);
+   for (i = 0; i < ranges.size(); i++) flatlat[i] = srclat + cos(azimuth * deg2rad) * ranges[i] / 111111;
 
-    returnPair = Cpath_topoSRTM1(plat, plon);
-    olat = returnPair.first;
-    olon = returnPair.second;
-    myFile.open("igppoutput.txt");
-    myFile << "OLAT [" << olat.size() << "] VALUES ARE: " << "\n";
-    for(vector<double>::iterator it = olat.begin(); it != olat.end(); it++) myFile << std::setprecision(5) << *it << "\t\t";
-    myFile << "\n" << "\n" << "OLON [" << olon.size() << "] VALUES ARE: " << "\n";
-    for(vector<double>::iterator it = olon.begin(); it != olon.end(); it++) myFile << std::setprecision(5) << *it << "\t\t";
-    myFile << "\n" << "\n" << "PLAT [" << plat.size() << "] VALUES ARE: " << "\n";
-    for(vector<double>::iterator it = plat.begin(); it != plat.end(); it++) myFile << std::setprecision(5) << *it << "\t\t";
-    myFile << "\n" << "\n" << "PLON [" << plon.size() << "] VALUES ARE: " << "\n";
-    for(vector<double>::iterator it = plon.begin(); it != plon.end(); it++) myFile << std::setprecision(5) << *it << "\t\t";
-    myFile.close();
-    cout << "Calculation finished. Output values are written to igppoutput.txt" << endl;
-    return 0;
+   returnPair = Cpath_topoSRTM1(plat, plon);
+   olat = returnPair.first;
+   olon = returnPair.second;
+   myFile.open("igppoutput.txt");
+   myFile << "OLAT [" << olat.size() << "] VALUES ARE: " << "\n";
+   for(vector<double>::iterator it = olat.begin(); it != olat.end(); it++) myFile << std::setprecision(5) << *it << "\t\t";
+   myFile << "\n" << "\n" << "OLON [" << olon.size() << "] VALUES ARE: " << "\n";
+   for(vector<double>::iterator it = olon.begin(); it != olon.end(); it++) myFile << std::setprecision(5) << *it << "\t\t";
+   myFile << "\n" << "\n" << "PLAT [" << plat.size() << "] VALUES ARE: " << "\n";
+   for(vector<double>::iterator it = plat.begin(); it != plat.end(); it++) myFile << std::setprecision(5) << *it << "\t\t";
+   myFile << "\n" << "\n" << "PLON [" << plon.size() << "] VALUES ARE: " << "\n";
+   for(vector<double>::iterator it = plon.begin(); it != plon.end(); it++) myFile << std::setprecision(5) << *it << "\t\t";
+   myFile.close();
+   cout << "Calculation finished. Output values are written to igppoutput.txt" << endl;
+   return 0;
 }
--- ccode/main/Cpath_topoSRTM1.cpp
+++ ccode/main/Cpath_topoSRTM1.cpp
@@ -23,7 +23,7 @@ pair<vector<double>, vector<double>> Cpath_topoSRTM1(vector<double> path_lat, ve
     long fileSize;
     double nbytes_per_lon;
     vector<int> db_loc{39, 43, 247, 250}, db_size{14401, 10801};
-    vector<double> glon(db_size[1]), glat(db_size[0]), olon(len), olat(len);
+    vector<double> glon(db_size[1]), glat(db_size[0]), olon(len), olat(len), path_data(path_lon.size());
     vector<int> ilat(len), ilon(len), ilatplus1(len), ilonplus1(len), minLat(len), minLon(len), offset(len);
     // tuple<vector<double>, vector<double>, vector<double>> returnTuple;
     pair<vector<double>, vector<double>> returnPair;
@@ -36,9 +36,14 @@ pair<vector<double>, vector<double>> Cpath_topoSRTM1(vector<double> path_lat, ve
         ilon[i] = round((path_lon[i] - db_loc[2])/db_res);
         offset[i] = ilon[i] * nbytes_per_lon + ilat[i] * 2;
     }
-/*
-    ifstream pFile("N39W113_4X3.hgt", std::ios::in | std::ios::binary);
-    if(!pFile) {
+
+    char* buffer;
+    FILE* pFile;
+    long lSize;
+    pFile = fopen("N39W113_4X3.hgt", "r+");
+    lSize = ftell(pFile);
+    buffer = (char*) malloc (sizeof(char)*lSize);
+    if(pFile == NULL) {
         cout << "Error opening file" << endl;
         vector<double> zeros1(path_lat.size());
         vector<double> zeros2(path_lat.size());
@@ -52,40 +57,14 @@ pair<vector<double>, vector<double>> Cpath_topoSRTM1(vector<double> path_lat, ve
         returnPair.second = zeros2;
         return returnPair;
     }
-    vector<double> path_data(path_lon.size());
-    */
-    /*unsigned char buffer[2];
-    short height[SRTM_SIZE][SRTM_SIZE] = {0};
-    for (i = 0; i < SRTM_SIZE; ++i) {
-        for (j = 0; j < SRTM_SIZE; ++j) {
-            if(!pFile.read( reinterpret_cast<char*>(buffer), sizeof(buffer))) {
-                cout << "Error opening file" << endl;
-                vector<double> zeros1(path_lat.size());
-                vector<double> zeros2(path_lat.size());
-                // vector<double> zeros3(path_lat.size());
-                // tuple<vector<double>, vector<double>, vector<double>> zeroTuple;
-                // std::get<0>(zeroTuple) = zeros1;
-                // std::get<1>(zeroTuple) = zeros2;
-                // std::get<2>(zeroTuple) = zeros3;
-                // return zeroTuple;
-                returnPair.first = zeros1;
-                returnPair.second = zeros2;
-                return returnPair;
-            }
-            height[i][j] = (buffer[0] << 8) | buffer[1];
-        }
-    }*/
+    for (i = 0; i < path_lon.size(); i++) {
+        fseek(pFile, offset[i], SEEK_SET); // SEEK_SET == bof
+        path_data[i] = fread(buffer, 2, 1, pFile);
+    }
 
-    // for (i = 0; i < path_lon.size(); i++) {
-    //pFile.seekg(offset[i], pFile.beg); // SEEK_SET == bof // might need to divide offset by 2
-   // pFile.read( reinterpret_cast<char*>(buffer), sizeof(buffer));
-    //short single_value = (buffer[0] << 8) | buffer[1];
-    //std::cout << "values at " << row << "," << col << ":" << std::endl;
-    //std::cout << "  height array: " << height[row][col] << ", file: " << single_value << std::endl;
-    // }
-    // fread(offset[i], 2,1,fid);
-
-    //pFile.close();
+    fclose(pFile);
+    free(buffer);
+  
     for (i = 0; i < db_size[1]; i++) glon[i] = (i + 1) * db_res - hdb_res;
     for (int j = 0; j < db_size[0]; j++) glat[j] = 90 - (j + 1) * db_res + hdb_res;
     for (int k = 0; k < ilat.size(); k++) ilatplus1[k] = ilat[k] + 1;
@@ -103,43 +82,50 @@ pair<vector<double>, vector<double>> Cpath_topoSRTM1(vector<double> path_lat, ve
     return returnPair;
 }
 
-// int main() {
-//     int i = 0, j = 0;
-//     ofstream myFile;
-//     double azimuth = 70, maxrange = 130000, max_range, range = 160000, rinc = 100, srclat = 41.131, srclon = 360 - 112.8965;
-//     vector<double> flatlat(1302), flatlon(1302), olat(1302), olon(1302), plat(1302), plon(1302), ranges(1302), tempRanges(1302);
-//     pair<vector<double>, vector<double>> tempValues;
-//     // tuple<vector<double>, vector<double>, vector<double>> returnTuple;
-//     pair<vector<double>, vector<double>> returnPair;
+int main() {
+    int i = 0, j = 0;
+    ofstream myFile;
+    double azimuth = 70, maxrange = 130000, max_range, range = 160000, rinc = 100, srclat = 41.131, srclon = 360 - 112.8965;
+    vector<double> flatlat(1302), flatlon(1302), olat(1302), olon(1302), path_data(1302), plat(1302), plon(1302), ranges(1302), tempRanges(1302);
+    pair<vector<double>, vector<double>> tempValues;
+    // tuple<vector<double>, vector<double>, vector<double>> returnTuple;
+    pair<vector<double>, vector<double>> returnPair;
 
-//     max_range = min(maxrange, range);
-//     while(j <= max_range + rinc) {
-//         ranges[i] += j;
-//         j += rinc;
-//         i++;
-//     } 
+    max_range = min(maxrange, range);
+    while(j <= max_range + rinc) {
+        ranges[i] += j;
+        j += rinc;
+        i++;
+    } 
 
-//     vector<double> azm(ranges.size(), 1 * azimuth);
-//     tempRanges = elementWiseDivision(ranges, 1852);
-//     tempValues = cGetLatLon(srclat, srclon, tempRanges, azm, 6378206.4, 0.006768658);
-//     plat = tempValues.first;
-//     plon = tempValues.second;
-//     for (i = 0; i < plon.size(); i++) plon[i] = cSetMinMax(plon[i], 0, 360);
-//     for (i = 0; i < ranges.size(); i++) flatlon[i] = srclon + sin(azimuth * deg2rad) * ranges[i] / (cos(srclat * deg2rad) * 111111);
-//     for (i = 0; i < ranges.size(); i++) flatlat[i] = srclat + cos(azimuth * deg2rad) * ranges[i] / 111111;
+    vector<double> azm(ranges.size(), 1 * azimuth);
+    tempRanges = elementWiseDivision(ranges, 1852);
+    tempValues = cGetLatLon(srclat, srclon, tempRanges, azm, 6378206.4, 0.006768658);
+    plat = tempValues.first;
+    plon = tempValues.second;
+    for (i = 0; i < plon.size(); i++) plon[i] = cSetMinMax(plon[i], 0, 360);
+    for (i = 0; i < ranges.size(); i++) flatlon[i] = srclon + sin(azimuth * deg2rad) * ranges[i] / (cos(srclat * deg2rad) * 111111);
+    for (i = 0; i < ranges.size(); i++) flatlat[i] = srclat + cos(azimuth * deg2rad) * ranges[i] / 111111;
 
-//     returnPair = Cpath_topoSRTM1(plat, plon);
-//     olat = returnPair.first;
-//     olon = returnPair.second;
-//     cout << "Received olat and olon values" << endl;
-//     cout << "Creating file" << endl;
-//     myFile.open("cpathoutput.txt");
-//     cout << "Writing olat values, please wait" << endl;
-//     myFile << "OLAT VALUES ARE: " << endl;
-//     for(vector<double>::iterator it = olat.begin(); it != olat.end(); it++) myFile << *it << " ";
-//     cout << "Writing olon values, please wait" << endl;
-//     myFile << endl << endl << "OLON VALUES ARE: " << endl;
-//     for(vector<double>::iterator it = olon.begin(); it != olon.end(); it++) myFile << *it << " ";
-//     myFile.close();
-//     return 0;
-// }
+    returnPair = Cpath_topoSRTM1(plat, plon);
+    olat = returnPair.first;
+    olon = returnPair.second;
+    // returnTuple = Cpath_topoSRTM1(plat, plon);
+    // path_data = std::get<0>(returnTuple);
+    // olat = std::get<1>(returnTuple);
+    // olon = std::get<2>(returnTuple);
+    cout << "Received path_data, olat, and olon values" << endl;
+    cout << "Creating file" << endl;
+    myFile.open("cpathoutput.txt");
+    cout << "Writing olat values, please wait" << endl;
+    myFile << "OLAT VALUES ARE: " << endl;
+    for(vector<double>::iterator it = olat.begin(); it != olat.end(); it++) myFile << *it << " ";
+    cout << "Writing olon values, please wait" << endl;
+    myFile << endl << endl << "OLON VALUES ARE: " << endl;
+    for(vector<double>::iterator it = olon.begin(); it != olon.end(); it++) myFile << *it << " ";
+    // cout << "Writing path_data values, please wait" << endl;
+    // myFile << endl << endl << "PATH_DATA VALUES ARE: " << endl;
+    // for (vector<double>::iterator it = path_data.begin(); it != path_data.end(); i++) myFile << *it << " ";
+    myFile.close();
+    return 0;
+}
